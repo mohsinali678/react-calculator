1e156c4ffe2506ea2b37ec42c7ca71a12c91e61a
Mohsin Tahir <mohsintahir@Mohsins-MBP.home>
2021-08-05 20:28:04 -0400

    Fixed comma bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import './App.css';
 import Input from "./Components/Input";
 import Button from "./Components/Button";
@@ -6,168 +6,196 @@ import Operators from "./Components/Operators";
 import Peripherals from "./Components/Peripherals";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      input:"0",
-      currentNumber:"0",
-      previousNumber:"",
-      operator:""
+      input: "0",
+      currentNumber: "0",
+      previousNumber: "",
+      operator: ""
     };
   }
   // Number with Commas
-  
-  
+
+
   // Add Value to Input
   addToInput = inputValue => {
-    let {input} = this.state;
+    let { input } = this.state;
     input = input.replaceAll(",", "");
-    if(input !== "0"){
-      let number = input + inputValue;
+    if (input.length <= 9) {
+      if (input.length !== 0) {
+        let number = input + inputValue;
+        if (number.length === 8) {
+          this.setState({
+            input: (Number(number).toExponential()),
+            currentNumber: (Number(number))
+          });
+        } else {
+          this.setState({
+            input: (Number(number).toLocaleString().toString()),
+            currentNumber: (Number(number))
+          });
+        }
+      } else {
+        this.setState({
+          input: inputValue,
+          currentNumber: inputValue
+        });
+      }
+    } else {
       this.setState({
-        input : (Number(number).toLocaleString().toString()),
-        currentNumber : (Number(number).toLocaleString().toString())
-      });
-    }else{
-      this.setState({
-        input : inputValue,
-        currentNumber : inputValue
+        input: "Out of bound",
       });
     }
   };
-  
+
   //Add Decimal When there is no decimal point
   addDecimal = inputValue => {
-    const {input} = this.state;
-    if(input.toString().indexOf(".") === -1){
-      this.setState({input : input + inputValue})
+    const { input } = this.state;
+    if (input.toString().indexOf(".") === -1) {
+      this.setState({ input: input + inputValue })
     }
   }
 
   // Add zero when input is not empty
   addZero = inputValue => {
-    const {input} = this.state;
-    if(input !== "0"){
-      this.setState({input: input + inputValue})
+    let { input } = this.state;
+    input = input.replaceAll(",", "");
+    if (input.length <= 9) {
+    if (input.toString() === "0") {
+      this.setState({ input })
+    }else{
+      let number = input + inputValue;
+      this.setState({ input: (Number(number).toLocaleString().toString()), currentNumber: (Number(number)) })
     }
+  }else{
+    this.setState({
+      input: "Out of bound",
+    });
+  }
   }
 
   //  Clear Current Input
   clearEntry = () => {
-    this.setState({input:"0", currentNumber:"0"})
+    this.setState({ input: "0", currentNumber: "0" })
   }
 
   //  Reset Everything
   allClear = () => {
     this.setState({
-      input:"0",
-      currentNumber:this.state.input,
-      previousNumber:"",
-      operator:""
+      input: "0",
+      currentNumber: this.state.input.replaceAll(",", ""),
+      previousNumber: "",
+      operator: ""
     })
   }
-  
+
   // Addition
   addition = () => {
-    
+
     this.setState({
-      previousNumber:this.state.currentNumber,
-      currentNumber : "",
-      input:"",
-      operator:"plus"
+      previousNumber: this.state.input.replaceAll(",", ""),
+      currentNumber: "",
+      input: "",
+      operator: "plus"
     })
-    
+
   }
 
   // Subtraction
   subtraction = () => {
     this.setState({
-      previousNumber:this.state.input,
-      input:"",
-      operator:"subtract"
+      previousNumber: this.state.input.replaceAll(",", ""),
+      input: "",
+      operator: "subtract"
     })
   }
 
   // Multiplication
   multiplication = () => {
     this.setState({
-      previousNumber:this.state.input,
-      input:"",
-      operator:"multiply"
+      previousNumber: this.state.input.replaceAll(",", ""),
+      input: "",
+      operator: "multiply"
     })
   }
 
   // Division
   division = () => {
     this.setState({
-      previousNumber:this.state.input,
-      input:"",
-      operator:"divide"
+      previousNumber: this.state.input.replaceAll(",", ""),
+      input: "",
+      operator: "divide"
     })
   }
 
   // Percentage
   percentage = () => {
     this.setState({
-      input: (parseInt(this.state.input) / 100),
+      input: (parseInt(this.state.input.replaceAll(",", "")) / 100),
     })
   }
 
-  
+
 
   // Positive to Negative and vice versa
   signChanger = () => {
-    if(this.state.input !== "0"){
+    if (this.state.input !== "0") {
       this.setState({
-        input:parseInt(this.state.input) * -1
+        input: parseInt(this.state.input) * -1
       })
     }
   }
 
   // Evalute Expression
   evaluate = () => {
-    let {currentNumber, previousNumber, operator} = this.state;
+    let { currentNumber, previousNumber, operator } = this.state;
 
-    
-    switch(true){
-      case operator === "plus":
-        this.setState({ 
-          input : parseInt(previousNumber) + parseInt(currentNumber),
-          currentNumber : parseInt(previousNumber) + parseInt(currentNumber)
-         });
-        break;
-      case operator === "subtract":
-        this.setState({ 
-          input : parseInt(previousNumber) - parseInt(currentNumber),
-          currentNumber : parseInt(previousNumber) - parseInt(currentNumber)
-         })
-        break;
-      case operator === "multiply":
-        this.setState({ 
-          input : parseInt(previousNumber) * parseInt(currentNumber),
-          currentNumber : parseInt(previousNumber) * parseInt(currentNumber)
-         })
-        break;
-      case operator === "divide":
-        this.setState({ 
-          input : parseInt(previousNumber) / parseInt(currentNumber),
-          currentNumber : parseInt(previousNumber) / parseInt(currentNumber)
-         })
-        break;
-      default:
-        this.setState({ input: "NaN", currentNumber : "NaN"})
+    if (currentNumber.toString().length <= 10) {
+      switch (true) {
+        case operator === "plus":
+          this.setState({
+            input: (parseInt(previousNumber) + parseInt(currentNumber)).toLocaleString().toString(),
+            currentNumber: parseInt(previousNumber) + parseInt(currentNumber)
+          });
+          break;
+        case operator === "subtract":
+          this.setState({
+            input: (parseInt(previousNumber) - parseInt(currentNumber)).toLocaleString().toString(),
+            currentNumber: parseInt(previousNumber) - parseInt(currentNumber)
+          })
+          break;
+        case operator === "multiply":
+          this.setState({
+            input: (parseInt(previousNumber) * parseInt(currentNumber)).toLocaleString().toString(),
+            currentNumber: parseInt(previousNumber) * parseInt(currentNumber)
+          })
+          break;
+        case operator === "divide":
+          this.setState({
+            input: (parseInt(previousNumber) / parseInt(currentNumber)).toLocaleString().toString(),
+            currentNumber: parseInt(previousNumber) / parseInt(currentNumber)
+          })
+          break;
+        default:
+          this.setState({ input: "NaN", currentNumber: "NaN" })
+      }
+    }else{
+      this.setState({
+        input: "Out of bound",
+      });
     }
   }
-  
-  render (){
-    const {input, currentNumber} = this.state;
+
+  render() {
+    const { input, currentNumber } = this.state;
     return (
       <div className="App">
-        <div style={{margin: "20px"}}>Project: <strong> React Calculator </strong> <br/><br/> Designed and Developed by:
+        <div style={{ margin: "20px" }}>Project: <strong> React Calculator </strong> <br /><br /> Designed and Developed by:
 
-        <strong>Hafiz Mohsin Ali</strong></div>
-        
+          <strong>Hafiz Mohsin Ali</strong></div>
+
         <div className="calc-wrapper">
 
           <div className="row">
@@ -175,12 +203,12 @@ class App extends Component {
           </div>
 
           <div className="row">
-            
+
             {
-            currentNumber === "0" ?
-            <Peripherals handleClick={this.allClear}>AC</Peripherals>
-            :
-            <Peripherals handleClick={this.clearEntry}>CE</Peripherals>
+              currentNumber === "0" ?
+                <Peripherals handleClick={this.allClear}>AC</Peripherals>
+                :
+                <Peripherals handleClick={this.clearEntry}>CE</Peripherals>
             }
 
             <Peripherals handleClick={this.signChanger}>±</Peripherals>
